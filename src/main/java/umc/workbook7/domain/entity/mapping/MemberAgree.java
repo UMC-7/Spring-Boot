484a165cd88550d86f8410109e0bd678d66c60c5
JH Park <harryest@naver.com>
2024-11-03 23:26:25 +0900

    Feat: MemberAgree mapping entity 생성

--- src/main/java/umc/workbook7/domain/entity/mapping/MemberAgree.java
+++ src/main/java/umc/workbook7/domain/entity/mapping/MemberAgree.java
@@ -1,4 +1,34 @@
 package umc.workbook7.domain.entity.mapping;
 
-public class MemberAgree {
+import jakarta.persistence.*;
+import lombok.*;
+import umc.workbook7.domain.common.BaseEntity;
+import umc.workbook7.domain.entity.Member;
+import umc.workbook7.domain.entity.Terms;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class MemberAgree extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberAgreeId;
+
+    //private Long memberId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    //private Long termsId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "terms_id")
+    private Terms terms;
+
+    // created_at, updated_at in BaseEntity
+
 }
+
+

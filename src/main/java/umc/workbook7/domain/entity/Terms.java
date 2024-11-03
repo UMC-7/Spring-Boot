d23cac3afe7578f09f67d7a292d338f562698979
JH Park <harryest@naver.com>
2024-11-03 23:22:55 +0900

    Feat: Terms entity 생성

--- src/main/java/umc/workbook7/domain/entity/Terms.java
+++ src/main/java/umc/workbook7/domain/entity/Terms.java
@@ -1,4 +1,35 @@
 package umc.workbook7.domain.entity;
 
-public class Terms {
+import jakarta.persistence.*;
+import lombok.*;
+import umc.workbook7.domain.common.BaseEntity;
+import umc.workbook7.domain.entity.mapping.MemberAgree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Terms extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long termId;
+
+    @Column(nullable = false, length = 20)
+    private String title;
+
+    private String body;
+
+    private Boolean optional;
+
+    // created_at, updated_at in BaseEntity
+
+    @OneToMany(mappedBy = "terms", cascade = CascadeType.ALL)
+    private List<MemberAgree> memberAgreeList = new ArrayList<>();
 }
+
+

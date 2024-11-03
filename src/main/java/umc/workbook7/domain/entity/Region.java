ad4271118ff7fd27e03ea12f972f725619da3ddf
JH Park <harryest@naver.com>
2024-11-03 23:20:50 +0900

    Feat: Region entity 생성

--- src/main/java/umc/workbook7/domain/entity/Region.java
+++ src/main/java/umc/workbook7/domain/entity/Region.java
@@ -1,4 +1,30 @@
 package umc.workbook7.domain.entity;
 
-public class Region {
+import jakarta.persistence.*;
+import lombok.*;
+import umc.workbook7.domain.common.BaseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Region extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long regionId;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    @OneToMany(mappedBy = "region", cascade = CascadeType.ALL)
+    private List<Store> storeList = new ArrayList<>();
+
+    // created_at, updated_at 은 BaseEntity에 있음
 }
+
+

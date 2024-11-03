99e903f511256f6b0f59b6a48d1099caeee0ecd8
JH Park <harryest@naver.com>
2024-11-03 23:23:25 +0900

    Feat: Uuid entity 생성

--- src/main/java/umc/workbook7/domain/entity/Uuid.java
+++ src/main/java/umc/workbook7/domain/entity/Uuid.java
@@ -1,4 +1,21 @@
 package umc.workbook7.domain.entity;
 
-public class Uuid {
+import jakarta.persistence.*;
+import lombok.*;
+import umc.workbook7.domain.common.BaseEntity;
+
+@Entity
+@Builder
+@Getter
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Uuid extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String uuid;
 }
+

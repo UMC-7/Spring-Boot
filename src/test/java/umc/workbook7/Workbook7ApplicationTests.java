fd2afdb4cd8e2509ede636054b650fd74d9c8884
JH Park <harryest@naver.com>
2024-11-03 22:23:29 +0900

    Feat: Project Initialize

---
+++ src/test/java/umc/workbook7/Workbook7ApplicationTests.java
@@ -0,0 +1,13 @@
+package umc.workbook7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workbook7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

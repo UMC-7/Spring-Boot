6b6c8ab87b7cc54fdf40461a723f95facbea5a67
JH Park <harryest@naver.com>
2024-11-20 08:29:42 +0900

    Feat: Merge wayne into week08/wayne

---
+++ src/main/java/umc/workbook7/Workbook7Application.java
@@ -0,0 +1,15 @@
+package umc.workbook7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workbook7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Workbook7Application.class, args);
+		System.out.println("[Initiate UMC 7th Workbook_Wayne]");
+	}
+
+}
+`

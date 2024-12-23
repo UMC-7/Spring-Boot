13af030f4d505328ae8cbf2d9c7c9f1feee75a8d
JH Park <harryest@naver.com>
2024-12-23 20:49:14 +0900

    Feat: 로그인 구현

--- src/main/java/umc/workbook7/repository/MemberRepository.java
+++ src/main/java/umc/workbook7/repository/MemberRepository.java
@@ -3,5 +3,8 @@ package umc.workbook7.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import umc.workbook7.domain.entity.Member;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
 }
